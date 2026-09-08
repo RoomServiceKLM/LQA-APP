9e23c3cc9edc5638db374fd39a7681f7405235d5
RoomServiceKLM <roomservicekimptonlosmonteros@gmail.com>
2026-09-08 23:32:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lqa-glass-v4';
+const CACHE_NAME = 'lqa-glass-v5';
 const urlsToCache = ['./','./index.html','./app.js','./manifest.json','./logo.svg','./logo192.png','./logo512.png'];
 
 self.addEventListener('install', e => {
